d4151057cb79923bdea42fea9dd29430021045e5
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-04-01 16:26:36 +0300

    Update DinosaurRunner.js

--- DinosaurRunner.js
+++ DinosaurRunner.js
@@ -6,25 +6,25 @@ let crashCounter = 0;
 let points = 0;
 let gameOver = false;
 
-function showDinosaur() {
+function startGame() {
     let dinosaur = {
         x: 300,
         y: 240,
         width: 20,
         height: 60,
         color: 'black',
-        speed: 6,
+        speed: 5,
         draw: function() {
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         },
         update: function() { 
-            if (dinosaur.y > 84 && dinosaurDirection === 1) {
+            if (dinosaur.y > 85 && dinosaurDirection === 1) {
                 dinosaur.y -= dinosaur.speed;
-            } else if (dinosaur.y === 84) {
+            } else if (dinosaur.y === 85) {
                 dinosaurDirection *= -1;
                 dinosaur.y += dinosaur.speed;
-            } else if (dinosaur.y > 84 && dinosaur.y < 240 && dinosaurDirection === -1) {
+            } else if (dinosaur.y > 85 && dinosaur.y < 240 && dinosaurDirection === -1) {
                 dinosaur.y += dinosaur.speed;
             }
         }
@@ -57,52 +57,17 @@ function showDinosaur() {
         randomObstacles = Math.floor(Math.random() * 2) + 1;
         console.log(randomObstacles);
         if (randomObstacles % 2 === 0) {
-            let obstacle = {
-                x: 1520,
-                y: 270, 
-                width: 50,
-                height: 35,
-                speed: 4,
-                draw: function() {
-                    ctx.fillStyle = this.color;
-                    ctx.fillRect(this.x, this.y, this.width, this.height);
-                },
-                update: function() {
-                    if (this.x > -100) {
-                        this.x -= this.speed; 
-                    } else {
-                        delete obstacle;
-                    }
-                }
-            }
-
-            function animateObstacles() {
-                if (obstacle.x < dinosaur.x + dinosaur.width &&
-                    obstacle.x + obstacle.width > dinosaur.x &&
-                    obstacle.y < dinosaur.y + dinosaur.height &&
-                    obstacle.y + obstacle.height > dinosaur.y) {
-                        ++crashCounter;
-                        ctx.clearRect(0, 0, canvas.width, canvas.height);
-                        if (crashCounter === 1) {
-                            gameOver = true;
-                            alert('Game OVER! You hit an obstacle! You have ' + points + ' points! REFRESH THE PAGE TO START A NEW GAME.');
-                        } 
-                } else if (obstacle.x < dinosaur.x && pointsCounter === 0) {
-                    points += 10;
-                    pointsCounter = 1;
-                }
-                requestAnimationFrame(animateObstacles);
-                ctx.clearRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
-                obstacle.update();
-                obstacle.draw();
-            }
-            if (gameOver === false) {
-                animateObstacles();
-            }
+            let yValue = 270;
+            createAndAnimateObstacles(yValue);
         } else if (randomObstacles % 2 != 0) {
+            let yValue = 230;
+            createAndAnimateObstacles(yValue);
+        }
+        
+        function createAndAnimateObstacles(yValue) {
             let obstacle = {
                 x: 1520,
-                y: 230, 
+                y: yValue, 
                 width: 50,
                 height: 35,
                 speed: 4,
@@ -142,9 +107,16 @@ function showDinosaur() {
             if (gameOver === false) {
                 animateObstacles();
             }
-        }  
+        }
     }
-    setInterval(showObstacles, 2500);
+    
+    if (points < 200) {
+        setInterval(showObstacles, 2500);
+    } else if (points >= 200 && points <= 500) {
+        setInterval(showObstacles, 2000);
+    } else {
+        setInterval(showObstacles, 1500);
+    }
 
     function incrementSeconds() {
         ++points;
